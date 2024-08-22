e3bb22b838ac10b365ea41dfa219d8b685b135b9
Tom Wade <twade7@gmail.com>
2024-08-22 12:29:32 +0100

    Fix InfernalRiftBelow deployment script

--- script/DeployBelow.s.sol
+++ script/DeployBelow.s.sol
@@ -3,23 +3,32 @@ pragma solidity ^0.8.26;
 
 import 'forge-std/Script.sol';
 
-import {InfernalRiftAbove} from '../src/InfernalRiftAbove.sol';
+import {ERC1155Bridgable} from '../src/libs/ERC1155Bridgable.sol';
+import {ERC721Bridgable} from '../src/libs/ERC721Bridgable.sol';
+import {InfernalRiftBelow} from '../src/InfernalRiftAbove.sol';
 
 
 /**
  * Configures Above with information about Below.
  *
- * Sepolia: `forge script script/ConfigureAbove.s.sol:ConfigureAbove --rpc-url "https://eth-sepolia.g.alchemy.com/v2/_RDgKRq2wGVTLXJ98vHHDfjr_dfQzTkS" --broadcast -vvvv --optimize --optimizer-runs 10000 --legacy`
+ * Sepolia: `forge script script/DeployBelow.s.sol:DeployBelow --rpc-url "https://eth-sepolia.g.alchemy.com/v2/_RDgKRq2wGVTLXJ98vHHDfjr_dfQzTkS" --broadcast -vvvv --optimize --optimizer-runs 10000 --legacy`
  */
-contract ConfigureAbove is Script {
+contract DeployBelow is Script {
 
     function run() external {
         vm.startBroadcast(vm.envUint('DEV_PRIVATE_KEY'));
 
+        address _RELAYER_ADDRESS = 0x4200000000000000000000000000000000000007;
+        address _L2_CROSS_DOMAIN_MESSENGER = 0x4200000000000000000000000000000000000007;
         address _INFERNAL_RIFT_ABOVE = 0x14a85AE3ED5FF92635c30003352A0305D301AF40; // Mainnet Sepolia
-        address _INFERNAL_RIFT_BELOW = 0x3Fa5Ac97e12f6A0Ee4FeBdc794C9E0dD841a2e5f; // Base Sepolia
 
-        InfernalRiftAbove(_INFERNAL_RIFT_ABOVE).setInfernalRiftBelow(_INFERNAL_RIFT_BELOW);
+        InfernalRiftBelow below = new InfernalRiftBelow(_RELAYER_ADDRESS, _L2_CROSS_DOMAIN_MESSENGER, _INFERNAL_RIFT_ABOVE);
+
+        ERC1155Bridgable erc1155Bridgable = new ERC1155Bridgable(address(below));
+        ERC721Bridgable erc721Bridgable = new ERC721Bridgable('', '', address(below));
+
+        below.initializeERC1155Bridgable(address(erc1155Bridgable));
+        below.initializeERC721Bridgable(address(erc721Bridgable));
 
         vm.stopBroadcast();
     }
