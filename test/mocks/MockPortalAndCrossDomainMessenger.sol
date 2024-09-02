d994cfed5a6f719dcde21a45acf22687b1df0e49
apoorvlathey <latheyapoorv@gmail.com>
2024-09-02 19:13:16 +0400

    update failing tests

--- test/mocks/MockPortalAndCrossDomainMessenger.sol
+++ test/mocks/MockPortalAndCrossDomainMessenger.sol
@@ -11,14 +11,23 @@ pragma solidity ^0.8.0;
 import {IOptimismPortal} from "../../src/interfaces/IOptimismPortal.sol";
 import {ICrossDomainMessenger} from "../../src/interfaces/ICrossDomainMessenger.sol";
 
+import { Vm } from "forge-std/Vm.sol";
+
 contract MockPortalAndCrossDomainMessenger is IOptimismPortal, ICrossDomainMessenger {
+    address internal constant VM_ADDRESS = address(uint160(uint256(keccak256("hevm cheat code"))));
+    Vm internal constant vm = Vm(VM_ADDRESS);
 
     error CallFailure();
 
     function depositTransaction(address _to, uint256 , uint64 , bool , bytes memory _data)
         external
         payable {
+        // Calculate the aliased address
+        address aliasedSender = address(uint160(address(msg.sender)) + uint160(0x1111000000000000000000000000000000001111));
+        vm.stopPrank();
+        vm.startPrank(aliasedSender);
         (bool success, ) = _to.call(_data);
+        vm.stopPrank();
         if (! success) {
             revert CallFailure();
         }
